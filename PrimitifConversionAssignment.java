2248c1e48aa87cc0196063f3fd2bd2889746c672
NuttQS <106371427+nadqz@users.noreply.github.com>
2024-03-08 19:06:39 +0700

    Add files via upload

---
+++ PrimitifConversionAssignment.java
@@ -0,0 +1,9 @@
+class PrimitifConversionAssignment {
+	public static void main(String[] args) {
+		int i;
+		double d;
+		i = 10;
+		d = 1;
+		System.out.println("Nilai d : "+ d);
+	}
+}
---
+++ PrimitifConversionAssignment2.java
@@ -0,0 +1,9 @@
+class PrimitifConversionAssignment2 {
+    public static void main(String[] args) {
+        double d;
+        short s;
+        d = 1.2345;
+        s = (short) d;
+        System.out.println("Nilai d : " + s);
+    }
+}
