7c722250fd1615a98e3e733e36c49db9f68b146b
NuttQS <106371427+nadqz@users.noreply.github.com>
2024-03-08 19:09:02 +0700

    Delete AssignPrimitive.java

--- AssignPrimitive.java
+++
@@ -1,7 +0,0 @@
-public class AssignPrimitive {
-    public static void main(String[] args) {
-        double f = 2.32323;
-        short s = (short) f;
-        System.out.println("Hasil = " + s);
-    }
-}
