2101ed3080c1053b14d0b6a142ff0a5d15d638be
NuttQS <106371427+nadqz@users.noreply.github.com>
2024-03-08 19:11:54 +0700

    Add files via upload

---
+++ Pertemuan1/Praktikum2a/Assign.java
@@ -0,0 +1,24 @@
+public class Assign {
+	public static void main (String args[]) {
+		int x, y;
+		float z= 3.414f;
+		double w = 3.1415;
+		boolean truth = true;
+		char c;
+		String str;
+		String str1 = "bye";
+		c = 'A';
+		str = "Hi out here!";
+		x =6;
+		y = 1000;
+		
+		System.out.println("Nilai variabel x: " + x);
+        System.out.println("Nilai variabel y: " + y);
+        System.out.println("Nilai variabel z: " + z);
+        System.out.println("Nilai variabel w: " + w);
+        System.out.println("Nilai variabel truth: " + truth);
+        System.out.println("Nilai variabel c: " + c);
+        System.out.println("Nilai variabel str: " + str);
+        System.out.println("Nilai variabel str1: " + str1);
+	}
+}
---
+++ Pertemuan1/Praktikum2a/AssignPrimitive.java
@@ -0,0 +1,7 @@
+public class AssignPrimitive {
+    public static void main(String[] args) {
+        double f = 2.32323;
+        short s = (short) f;
+        System.out.println("Hasil = " + s);
+    }
+}
